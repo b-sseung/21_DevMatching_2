57a702c5f883eee809b22df5baf2f213b83242a0
user <kamo0915@gmail.com>
2022-08-22 17:19:01 +0900

    220822+17:18

--- src/CartPage.js
+++ src/CartPage.js
@@ -1,10 +1,58 @@
+import { request } from './api.js';
+import { getItem } from './storage.js';
+import { routeChange } from './router.js';
+import Cart from './Cart.js';
+
 export default function CartPage({ $target }) {
   const $page = document.createElement('div');
   $page.className = 'CartPage';
 
   $page.innerHTML = '<h1>장바구니</h1>'
 
-  this.render = () => {
-    $target.appendChild($page);
+  let cartComponent = null;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.render();
   }
+
+  const cartData = getItem('products_cart', []);
+  this.state = {
+    products: null
+  }
+
+  this.render = () => {
+    if (cartData.length === 0) {
+      alert('장바구니가 비어있습니다.')
+      routeChange('/');
+    } else {
+      $target.appendChild($page);
+      if (this.state.products && !cartComponent) {
+        cartComponent = new Cart({
+          $target: $page,
+          initState: this.state.products
+        })
+      }
+    }
+  }
+
+  this.fetchProducts = async () => {
+    const products = await Promise.all(cartData.map(async (cartItem) => {
+      const product = await request(`/products/${cartItem.productId}`);
+      const selectedOption = product.productOptions.find(option => option.id === cartItem.optionId);
+
+      return {
+        imageUrl: product.imageUrl,
+        productName: product.name,
+        quantity: cartItem.quantity,
+        productPrice: product.price,
+        optionName: selectedOption.name,
+        optionPrice: selectedOption.price
+      }
+    }))
+
+    this.setState({ products });
+  }
+
+  this.fetchProducts();
 }
